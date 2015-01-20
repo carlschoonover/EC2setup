8065100dae180fb2adec1e9a5a80d612368e2bf9
Carl Schoonover <ceschoonover@gmail.com>
2015-01-19 20:06:26 -0500

    Update EC2setup.sh

--- EC2setup.sh
+++ EC2setup.sh
@@ -23,3 +23,7 @@ conda create -n klusta python=2.7 --yes
 conda install -n klusta scipy pandas=0.12 pytables=3.0 pyqt setuptools pip cython nose ipython-notebook matplotlib --yes
 conda install -n klusta numpy=1.8 --yes
 source activate klusta
+
+echo >> .bashrc
+echo source activate klusta >> .bashrc
+pip install pyopengl
