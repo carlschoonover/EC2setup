6381f5ef1a3d7d7a0ca759d92494f29b988d7717
Carl Schoonover <ceschoonover@gmail.com>
2015-01-19 19:25:40 -0500

    Create EC2setup.sh

---
+++ EC2setup.sh
@@ -0,0 +1,10 @@
+sudo apt-get update
+sudo apt-get install htop
+sudo apt-get install git
+sudo apt-get install build-essential 
+sudo apt-get install zip unzip
+sudo apt-get install mailutils
+# On 12/21/2014 there was a bug in klusta that meant it need an X11 server.
+# If necessary, install X.
+sudo apt-get install xorg
+sudo apt-get install openbox
