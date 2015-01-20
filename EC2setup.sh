5761d9bb19a1ea300a8d4ca6c87a08ed222c4e67
Carl Schoonover <ceschoonover@gmail.com>
2015-01-19 19:39:46 -0500

    Update EC2setup.sh

--- EC2setup.sh
+++ EC2setup.sh
@@ -3,7 +3,7 @@ sudo apt-get -y --force-yes install htop
 sudo apt-get -y --force-yes install git
 sudo apt-get -y --force-yes install build-essential 
 sudo apt-get -y --force-yes install zip unzip
-sudo apt-get -y --force-yes install mailutils
+# sudo apt-get -y --force-yes install mailutils
 # On 12/21/2014 there was a bug in klusta that meant it need an X11 server.
 # If necessary, install X.
 sudo apt-get -y --force-yes install xorg
