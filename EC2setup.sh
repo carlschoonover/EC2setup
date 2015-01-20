50a40e3968af466f5215880eb9e22d84a63bfc35
Carl Schoonover <ceschoonover@gmail.com>
2015-01-19 19:42:42 -0500

    Update EC2setup.sh

--- EC2setup.sh
+++ EC2setup.sh
@@ -1,3 +1,6 @@
+################
+### PACKAGES ###
+################
 sudo apt-get -y --force-yes update
 sudo apt-get -y --force-yes install htop
 sudo apt-get -y --force-yes install git
@@ -8,3 +11,16 @@ sudo apt-get -y --force-yes install zip unzip
 # If necessary, install X.
 sudo apt-get -y --force-yes install xorg
 sudo apt-get -y --force-yes install openbox
+
+#######################
+### PYTHON ANACONDA ###
+#######################
+mkdir ~/software
+cd ~/software
+wget http://repo.continuum.io/miniconda/Miniconda-3.7.0-Linux-x86_64.sh
+bash -b Miniconda-3.7.0-Linux-x86_64.sh
+exec bash
+conda create -n klusta python=2.7 --yes
+conda install -n klusta scipy pandas=0.12 pytables=3.0 pyqt setuptools pip cython nose ipython-notebook matplotlib --yes
+conda install -n klusta numpy=1.8 --yes
+source activate klusta
