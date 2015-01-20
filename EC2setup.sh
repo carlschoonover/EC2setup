e85f5636f87738384bcf7396fb6046d94c0498c6
Carl Schoonover <ceschoonover@gmail.com>
2015-01-19 19:36:33 -0500

    Update EC2setup.sh

--- EC2setup.sh
+++ EC2setup.sh
@@ -1,10 +1,10 @@
-sudo apt-get update
-sudo apt-get install htop
-sudo apt-get install git
-sudo apt-get install build-essential 
-sudo apt-get install zip unzip
-sudo apt-get install mailutils
+sudo apt-get -y --force-yes update
+sudo apt-get -y --force-yes install htop
+sudo apt-get -y --force-yes install git
+sudo apt-get -y --force-yes install build-essential 
+sudo apt-get -y --force-yes install zip unzip
+sudo apt-get -y --force-yes install mailutils
 # On 12/21/2014 there was a bug in klusta that meant it need an X11 server.
 # If necessary, install X.
-sudo apt-get install xorg
-sudo apt-get install openbox
+sudo apt-get -y --force-yes install xorg
+sudo apt-get -y --force-yes install openbox
