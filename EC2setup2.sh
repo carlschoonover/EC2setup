df3648ef79bc7b01ec0012f95fdedc875b1e06ce
Carl Schoonover <ceschoonover@gmail.com>
2015-01-20 13:19:39 -0500

    Update EC2setup2.sh

--- EC2setup2.sh
+++ EC2setup2.sh
@@ -1,3 +1,5 @@
+userName="kruncheruser"
+
 ##################
 ### CONDA VENV ###
 ##################
@@ -9,6 +11,38 @@ echo >> ~/.bashrc
 echo source activate klusta >> ~/.bashrc
 pip install pyopengl
 
-###################
-### KLUSTAVIEWA ###
-###################
+##############
+### KLUSTA ###
+##############
+# Klusta:  https://github.com/klusta-team/klustaviewa/releases/
+# Git:     https://github.com/klusta-team
+cd ~/software
+wget https://github.com/klusta-team/klustaviewa/releases/download/v0.3.0-rc1/klustaviewa-0.3.0.rc1.zip
+unzip klustaviewa-0.3.0.rc1.zip
+rm klustaviewa-0.3.0.rc1.zip
+# Pull the latest SD2 from the Git, and Kwiklib too
+wget https://github.com/klusta-team/kwiklib/archive/master.zip
+unzip master.zip 
+rm master.zip
+wget https://github.com/klusta-team/spikedetekt2/archive/master.zip
+unzip master.zip 
+rm master.zip
+rm -R ~/software/klustaviewa-0.3.0.rc1/spikedetekt2/
+rm -R ~/software/klustaviewa-0.3.0.rc1/kwiklib/
+cp -R ~/software/spikedetekt2-master/spikedetekt2/ ~/software/klustaviewa-0.3.0.rc1/
+cp -R ~/software/kwiklib-master/kwiklib/ ~/software/klustaviewa-0.3.0.rc1/
+cd ~/software/klustaviewa-0.3.0.rc1/
+python setup.py install
+
+##################
+### KLUSTAKWIK ###
+##################
+cd ~/software/
+git clone https://github.com/klusta-team/klustakwik.git
+cd ~/software/klustakwik/
+############################## as of 1/14/15, i think openmp is in master branch
+############################## git checkout unmaskedcov
+make
+mv KlustaKwik klustakwik 
+echo >> ~/.bashrc
+echo export PATH=\"/home/$userName/software/klustakwik/:\$PATH\" >> ~/.bashrc
