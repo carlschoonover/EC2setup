79c86dada1d54ba07087effd10026d3aceb90ced
Carl Schoonover <ceschoonover@gmail.com>
2015-01-19 19:54:20 -0500

    Create EC2setup2.sh

---
+++ EC2setup2.sh
@@ -0,0 +1,9 @@
+######################
+### Python Modules ###
+######################
+
+exec bash
+conda create -n klusta python=2.7 --yes
+conda install -n klusta scipy pandas=0.12 pytables=3.0 pyqt setuptools pip cython nose ipython-notebook matplotlib --yes
+conda install -n klusta numpy=1.8 --yes
+source activate klusta
