6f2e21af5a4737155254c32c497d6d1c3875853a
Carl Schoonover <ceschoonover@gmail.com>
2015-01-26 15:26:08 -0500

    Update SDshell.sh

--- SDshell.sh
+++ SDshell.sh
@@ -2,4 +2,4 @@ paramFileName=$(ls *.prm)
 outputFileName="${paramFileName%.*}_outputSD.txt"
 experimentName=$(grep -F "experiment_name =" params.prm)
 klusta $paramFileName --detect-only | tee $outputFileName
-echo "$experimentName SpikeDetekt is done running" | mail -s "Run completed" andrew.jp.fink@gmail.com ceschoonover@gmail.com
+echo "$experimentName SpikeDetekt is done running" | mail -s "SD run completed" andrew.jp.fink@gmail.com ceschoonover@gmail.com
