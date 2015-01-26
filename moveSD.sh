8330e39ffac0a0f31bcf74c7d388d6149daaf6ec
Carl Schoonover <carlschoonover@Carls-MacBook-Pro.local>
2015-01-26 15:56:59 -0500

    a

--- moveSD.sh
+++ moveSD.sh
@@ -1,12 +1,18 @@
-pemFile="~/current2.pem"
+pemFile="/home/kruncheruser/current2.pem"
 IPaddr="54.175.112.209"
 
-prbFile=$PWD'/'$(ls *.prb)
+highFile=$PWD'/'$(ls *.high.kwd)
+kwikFile=$PWD'/'$(ls *.kwik)
+kwxFile=$PWD'/'$(ls *.kwx)
 logFile=$PWD'/'$(ls *.log)
+lowFile=$PWD'/'$(ls *.low.kwd)
+rawFile=$PWD'/'$(ls *.raw.kwd)
+prbFile=$PWD'/'$(ls *.prb)
+prmOutputFile=$PWD'/'$(ls *.txt)
+prmFile=$PWD'/'$(ls *.prm)
 
-echo $pemFile
-echo $IPaddr
-echo $prbFile
-echo $logFile
+fileNames=ls -d -1 $PWD/*.* | grep -v '.dat'
 
-# scp -i $pemFile $prbFile $logFile kruncheruser@$IPaddr:/mnt/data/fooData/
+echo $fileNames
+
+$scp -i $pemFile $PWD/* kruncheruser@$IPaddr:/mnt/data/fooData/
