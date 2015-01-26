e907b0369f78d95b4774f775b7ab842e7a336b55
Carl Schoonover <ceschoonover@gmail.com>
2015-01-26 15:25:45 -0500

    Update KKshell.sh

--- KKshell.sh
+++ KKshell.sh
@@ -2,4 +2,4 @@ paramFileName=$(ls *.prm)
 outputFileName="${paramFileName%.*}_outputKK.txt"
 experimentName=$(grep -F "experiment_name =" params.prm)
 klusta $paramFileName --cluster-only | tee $outputFileName
-echo "$experimentName KlustaKwik is done running" | mail -s "Run completed" andrew.jp.fink@gmail.com ceschoonover@gmail.com
+echo "$experimentName KlustaKwik is done running" | mail -s "KK run completed" andrew.jp.fink@gmail.com ceschoonover@gmail.com
