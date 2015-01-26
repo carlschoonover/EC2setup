8c3f52a18a0eb85971b82418f43eee8925bcab3a
Carl Schoonover <carlschoonover@Carls-MacBook-Pro.local>
2015-01-26 16:29:00 -0500

    a

--- moveSD.sh
+++ moveSD.sh
@@ -1,11 +1,4 @@
 pemFile=$(readlink -f $1)
 IPaddr=$2
-#IPaddr="54.175.112.209"
-
-
-echo $pemFile
-
-fileNames=$(ls -d -1 $PWD/*.* | grep -v '.dat')
-#echo $fileNames
-
+fileNames=$(ls -d -1 $PWD/*.* | grep -v '\.dat')
 # scp -i $pemFile $fileNames kruncheruser@$IPaddr:/mnt/data/
