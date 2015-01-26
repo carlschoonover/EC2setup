82f65d4162b3a304a0612388081fa79085efb561
Carl Schoonover <ceschoonover@gmail.com>
2015-01-26 16:37:40 -0500

    Update moveSD.sh

--- moveSD.sh
+++ moveSD.sh
@@ -1,4 +1,4 @@
 pemFile=$(readlink -f $1)
 IPaddr=$2
 fileNames=$(ls -d -1 $PWD/*.* | grep -v '\.dat')
-# scp -i $pemFile $fileNames kruncheruser@$IPaddr:/mnt/data/
+scp -i $pemFile $fileNames kruncheruser@$IPaddr:/mnt/data/
