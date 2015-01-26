a9e6aee6ed7fed3f89535852dbcdcdf12d37f827
Carl Schoonover <ceschoonover@gmail.com>
2015-01-26 17:23:12 -0500

    Update EC2setup2.sh

--- EC2setup2.sh
+++ EC2setup2.sh
@@ -70,9 +70,10 @@ fi
 ###################
 ### KLUSTASHELL ###
 ###################
-cd /mnt/data
+cd ~/sh/
 shellFilesPath="https://raw.githubusercontent.com/carlschoonover/EC2setup/master/"
 wget -N $shellFilesPath"klustashell.sh" $shellFilesPath"SDshell.sh" $shellFilesPath"KKshell.sh" $shellFilesPath"moveSD.sh"
 chmod u+x klustashell.sh SDshell.sh KKshell.sh moveSD.sh
+cd ~/
 
 exec bash
