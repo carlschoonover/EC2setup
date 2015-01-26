1482eab5a62740b8202e57db2aafbb66035c21f8
Carl Schoonover <carlschoonover@Carls-MacBook-Pro.local>
2015-01-26 11:17:46 -0500

    a

--- klustashell.sh
+++ klustashell.sh
@@ -2,4 +2,4 @@ paramFileName=$(ls *.prm)
 outputFileName="${paramFileName%.*}_output.txt"
 experimentName=$(grep -F "experiment_name =" params.prm)
 klusta $paramFileName | tee $outputFileName
-echo "$experimentName is done running" | mail -s "Run completed" andrew.jp.fink@gmail.com ceschoonover@gmail.com
+echo "$experimentName is done running" | mail -s "Run completed" andrew.jp.fink@gmail.com ceschoonover@gmail.com an11@columbia.edu
