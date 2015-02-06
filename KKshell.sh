eaa2a426c903f914e38bfa03c97a72c32e5c27f7
Carl Schoonover <ceschoonover@gmail.com>
2015-02-06 14:23:55 -0500

    Update KKshell.sh

--- KKshell.sh
+++ KKshell.sh
@@ -11,9 +11,10 @@ if [ $# -eq 0 ]
 fi
 sed '/KK_Subset = /c \KK_Subset = '$1 -i $paramFileName
 
-# Run KlustaKwik
+# Run KlustaKwik and set files aside
 klusta $paramFileName --cluster-only | tee $outputFileName
-mv _klustakwik _klustakwik-subset$1
+mkdir subset-$1
+cp * subset-$1
 
 # Send confirmation email once all is done
 echo -e "$experimentName KlustaKwik KK_Subset $1 is done running\n\n$(tail -n 1 $outputFileName)" | mail -s "KlustaKwik KK_Subset $1 run completed on $(hostname)" andrew.jp.fink@gmail.com ceschoonover@gmail.com
