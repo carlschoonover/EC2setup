e4fd67660a599956f9a3cb67ff4420fcb407e7d9
Carl Schoonover <ceschoonover@gmail.com>
2015-02-06 13:08:51 -0500

    Update KKshell.sh

--- KKshell.sh
+++ KKshell.sh
@@ -1,7 +1,9 @@
+# Get filenames and parameters
 paramFileName=$(ls *.prm)
 outputFileName="output_KK.txt"
 experimentName=$(grep -F "experiment_name =" $paramFileName)
 
+# Set user-specified KK_Subset or halt if not entered
 if [ $# -eq 0 ]
   then
     echo "Please define KK_Subset. (Syntax: \""KKshell.sh 50\"" for KK_Subset = 50)"
@@ -9,5 +11,10 @@ if [ $# -eq 0 ]
 fi
 sed '/KK_Subset = /c \KK_Subset = '$1 -i $paramFileName
 
+# Run KlustaKwik
 klusta $paramFileName --cluster-only | tee $outputFileName
-echo -e "$experimentName KlustaKwik is done running\n\n$(tail -n 1 $outputFileName)" | mail -s "KK run completed on $(hostname)" andrew.jp.fink@gmail.com ceschoonover@gmail.com
+
+# Zip the required files
+
+# Send confirmation email once all is done
+echo -e "$experimentName KlustaKwik KK_Subset $1 is done running\n\n$(tail -n 1 $outputFileName)" | mail -s "KlustaKwik KK_Subset $1 run completed on $(hostname)" andrew.jp.fink@gmail.com ceschoonover@gmail.com
